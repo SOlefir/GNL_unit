efaa9abb98850ac106e60aa395815f2840fd310c
Sofiaa OLEFIR <solefir@e1r10p10.unit.ua>
2019-02-19 11:37:11 +0200

    storage, del_node, ft_gnl

--- gnl_new.c
+++ gnl_new.c
@@ -6,7 +6,7 @@
 /*   By: solefir <solefir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 14:17:10 by solefir           #+#    #+#             */
-/*   Updated: 2019/02/18 18:39:07 by solefir          ###   ########.fr       */
+/*   Updated: 2019/02/19 11:24:59 by solefir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,14 @@ static t_list		*find_fd(t_list *head, int fd)
 	return (temp);
 }
 
+t_list				*del_node(t_list *head)
+{
+	t_list	*temp;
+
+	temp = *head; /* удалить 1 node, зафришить. если есть знак, конец строки, то удалить контент до этого знака включительно и вернуть этот лист */
+	free(head);
+}
+
 static t_list		*write_in_line(t_list *head, size_t count, int fd, char **line)
 {
 	t_list		*temp;
@@ -32,25 +40,25 @@ static t_list		*write_in_line(t_list *head, size_t count, int fd, char **line)
 	temp = head;
 	k = 0;
 	*line = ft_strnew(count);
-	while (line = temp   )/*temp->next != NULL*/
+	while ((temp->content)[k] != '\n')
 		{
+			k = 0;
 			while (k != temp->content_size)
 				*line[i++] = (temp->content)[k++];
+			temp = temp->next;
+			head = del_node(head);
 		}
-	return(temp);
+	return(head);
 }
 
-static int			read_and_write(t_list *head, t_list **storage, const int fd, char **line)
+static int			read_and_write(t_list *head, char **buf, const int fd, int	*count)
 {
 	t_list	*temp;
 	char	*there;
-	char	*buf[BUFF_SIZE];
 	int		i;
-	int		count;
 
 	temp = head;
 	there = NULL;
-	count = 1;
 	while (i = read(fd, &buf, BUFF_SIZE) > 0 || there == NULL)
 	{
 		if (head == NULL)
@@ -62,27 +70,31 @@ static int			read_and_write(t_list *head, t_list **storage, const int fd, char *
 			{
 				temp->next = ft_lstnew(&buf, (size_t)i);
 				temp = temp->next;
-				count =* i;
+				*count =+ i;
 			}
-		there = ft_strchr(*buf, '\n');
+		there = ft_strchr(buf, '\n');
+		*count = *count - (i - (*buf + *there));
 	}
-	storage->next = write_in_line(head, count, fd, &line);
 	return (i);
 }
-
 
 int		get_next_line(const int fd, char **line)
 {
 	t_list			*buffer;
-	static	t_list	*storage = NULL; 
+	static t_list	*storage = NULL;
+	int				i;
+	int				*count[1];
+	char			*buf[BUFF_SIZE];
 
 	if (fd < 0 || !line || read(fd, 0, 0) < 0)
 		return (-1);
 	buffer = find_fd(storage, fd);
-	return (read_and_write(buffer, &storage, fd)));
+	i = read_and_write(buffer, &buf, fd, &count) != 0;
+	storage = write_in_line(buffer, count, fd, &line);
+	return (i);
 }
 
 /* я думаю, что решение должно быть простым в несколько крупных шагов
-	1. считать и записать в листы (подсчитывая их)
+	1. читать и записать в листы (подсчитывая их)
 	2. из листов записать в лайн удаляя их до последнего
 	3. сохранить в структуру фд и последний лист */
